743a15a70eab6dea753e231eee930dda3a118a38
Diether Noche <diether.noche@gmail.com>
2023-01-01 13:18:54 +0800

    Add star icons with colors

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,7 @@
 // Import our custom CSS
 import "../styles/styles.scss";
 import "../styles/styles.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
